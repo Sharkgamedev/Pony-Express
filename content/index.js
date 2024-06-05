0ee8301d387164a1c8f44fca299fe67ba31ed317
Sharkgamedev <owen.silva@sharksinteractive.net>
2024-06-05 00:31:10 -0400

    June th, 2024 \'Economic Spiral\!\' edition

--- content/index.js
+++ content/index.js
@@ -1,8 +1,9 @@
 export const articles = {
-    total: 3,
+    total: 4,
 
     0: '123022.json',
     1: '051423.json',
     2: '102323.json',
-    3: '122723.json'
+    3: '122723.json',
+    4: '060124.json'
 };
